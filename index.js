2d03bbb379bd4cdd818b243dfb5ba04bf99d6a9d
Matt Styles <matt@veryfizzyjelly.com>
2015-04-10 09:15:49 +0100

    #add optional asyncToGenerator babel option for testing

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-require( 'babel/register' );
+require( 'babel/register' )({
+    optional: [
+        'asyncToGenerator'
+    ]
+});
 
+/**
+ * Moniker-api is expected to be mounted into an app.
+ * This is useful for firing up a test instance though.
+ */
 
 var app = require( './lib/server' );
 var port = process.env.PORT || process.env.npm_package_config_port || 14320;
